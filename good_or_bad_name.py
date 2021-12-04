766cdf77527f2ffb2cd401addccf453dbd9bf867
mrtimotey <91370756+mrtimotey@users.noreply.github.com>
2021-12-04 13:32:56 +0300

    Add files via upload

---
+++ good_or_bad_name.py
@@ -0,0 +1,7 @@
+print(""" Хорошие названия переменных:
+age; name; vasya_ivanov;
+Плохие названия переменных:
+dfdf; vasya_ne_vernuvshiy_sotku_dolg; VanyaMashinu_net;
+Недопустимые названия переменных:
+2name; Alecsandr Tolstoy; danya,Milk;
+""")
