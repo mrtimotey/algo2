02c1e209ecb2a37174b3288091a6ca0f6d8c2ad4
mrtimotey <91370756+mrtimotey@users.noreply.github.com>
2021-12-25 13:02:26 +0300

    Add files via upload

---
+++ craps.py
@@ -0,0 +1,6 @@
+import  random 
+diel  =  random.randint(1, 6) 
+die2  =  random.randrange(6)  +  1 
+total  =  diel  +  die2 
+print("Пpи  вашем броске выпало", diel, "и", die2, "очков,  в  сумме", total) 
+input("\n\nHaжмитe Enter, чтобы выйти") 
